ae2a1c49aaa93b98354ab23125c71f063f1b8544
VitZz-git <vitorhugodossantos18042000@gmail.com>
2025-10-20 20:49:34 -0300

    Criado a Controller

---
+++ apam/src/main/java/dev/java10x/apam/ApamApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.apam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApamApplication.class, args);
+	}
+
+}
---
+++ apam/src/test/java/dev/java10x/apam/ApamApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.apam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
